a1658be3cdc375026994e4ce4ce93d829b77a781
Polash-Hemrom <code.hemrom@gmail.com>
2026-08-27 08:07:44 +0600

    Multiplication table printer

---
+++ Code Practice/loop/03___solution.py
@@ -0,0 +1,9 @@
+# 3. Multiplication Table Printer
+
+numbers = 10
+multiplication = 0
+
+while numbers > multiplication:
+    multiplication +=1
+    print(numbers, 'X', multiplication, '=', numbers * multiplication)
+     
